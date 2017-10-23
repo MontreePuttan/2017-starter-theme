6c7fc0d82b871655079c82455316ada3be31adf8
MontreeP <montree@mohara.co>
2017-10-23 23:28:22 +0700

    WordPress Navigation Menus (Theme Development)

--- wp-content/themes/learningWordpress/functions.php
+++ wp-content/themes/learningWordpress/functions.php
@@ -4,3 +4,9 @@ function learningWordPress_resources(){
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 }
 add_action('wp_enqueue_scripts','learningWordPress_resources');
+
+// Navigation Menus
+register_nav_menus(array(
+	'primary'=>__('Primary Menu'),
+	'footer'=>__('Footer Menu'),
+));
