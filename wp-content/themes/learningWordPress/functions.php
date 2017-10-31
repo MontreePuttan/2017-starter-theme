7e7ea88aff591250cb7b1a2060144b10d5405c68
MontreeP <montree@mohara.co>
2017-10-31 14:26:31 +0700

    WordPress REST API

--- wp-content/themes/learningWordPress/functions.php
+++ wp-content/themes/learningWordPress/functions.php
@@ -3,6 +3,12 @@
 function learningWordPress_resources() {
 	
 	wp_enqueue_style('style', get_stylesheet_uri());
+	wp_enqueue_script('main_js', get_template_directory_uri() . '/js/main.js', NULL, 1.0, true);
+
+	wp_localize_script('main_js', 'magicalData', array(
+		'nonce' => wp_create_nonce('wp_rest'),
+		'siteURL' => get_site_url()
+	));
 	
 }
 
@@ -200,10 +206,8 @@ function learningWordPress_customize_css() { ?>
 
 add_action('wp_head', 'learningWordPress_customize_css');
 
-
-// Add footer callout section to admin appreance customize screen
-function lwp_footer_callout($wp_customize){
-
+// Add Footer callout section to admin appearance customize screen
+function lwp_footer_callout($wp_customize) {
 	$wp_customize->add_section('lwp-footer-callout-section', array(
 		'title' => 'Footer Callout'
 	));
@@ -212,44 +216,53 @@ function lwp_footer_callout($wp_customize){
 		'default' => 'No'
 	));
 
-	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'lwp-footer-callout-display-control', array(
-		'label' => 'Display this section?',
-		'section' => 'lwp-footer-callout-section',
-		'settings' => 'lwp-footer-callout-display',
-		'type' => 'select',
-		'choices' => array('No' => 'No','Yes' => 'Yes')
-	) ) );
+	$wp_customize->add_control( new WP_Customize_Control($wp_customize, 'lwp-footer-callout-display-control', array(
+			'label' => 'Display this section?',
+			'section' => 'lwp-footer-callout-section',
+			'settings' => 'lwp-footer-callout-display',
+			'type' => 'select',
+			'choices' => array('No' => 'No', 'Yes' => 'Yes')
+		)));
 
-	$wp_customize->add_setting('lwp-footer-callout-text', array(
-		'default' => 'Example Paragraph Text!'
+	$wp_customize->add_setting('lwp-footer-callout-headline', array(
+		'default' => 'Example Headline Text!'
 	));
 
-	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'lwp-footer-callout-text-control', array(
-		'label' => 'Headline',
-		'section' => 'lwp-footer-callout-section',
-		'settings' => 'lwp-footer-callout-text',
-		'type' => 'textarea'
-	) ) );
+	$wp_customize->add_control( new WP_Customize_Control($wp_customize, 'lwp-footer-callout-headline-control', array(
+			'label' => 'Headline',
+			'section' => 'lwp-footer-callout-section',
+			'settings' => 'lwp-footer-callout-headline'
+		)));
+
+	$wp_customize->add_setting('lwp-footer-callout-text', array(
+		'default' => 'Example paragraph text.'
+	));
+
+	$wp_customize->add_control( new WP_Customize_Control($wp_customize, 'lwp-footer-callout-text-control', array(
+			'label' => 'Text',
+			'section' => 'lwp-footer-callout-section',
+			'settings' => 'lwp-footer-callout-text',
+			'type' => 'textarea'
+		)));
 
 	$wp_customize->add_setting('lwp-footer-callout-link');
 
-	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'lwp-footer-callout-link-control', array(
-		'label' => 'Link',
-		'section' => 'lwp-footer-callout-section',
-		'settings' => 'lwp-footer-callout-link',
-		'type' => 'dropdown-pages'
-	) ) );
+	$wp_customize->add_control( new WP_Customize_Control($wp_customize, 'lwp-footer-callout-link-control', array(
+			'label' => 'Link',
+			'section' => 'lwp-footer-callout-section',
+			'settings' => 'lwp-footer-callout-link',
+			'type' => 'dropdown-pages'
+		)));
 
 	$wp_customize->add_setting('lwp-footer-callout-image');
 
-	$wp_customize->add_control( new WP_Customize_Cropped_Image_Control( $wp_customize, 'lwp-footer-callout-image-control', array(
-		'label' => 'Image',
-		'section' => 'lwp-footer-callout-section',
-		'settings' => 'lwp-footer-callout-image',
-		'width' => 700,
-		'height' => 500
-	) ) );
-
+	$wp_customize->add_control( new WP_Customize_Cropped_Image_Control($wp_customize, 'lwp-footer-callout-image-control', array(
+			'label' => 'Image',
+			'section' => 'lwp-footer-callout-section',
+			'settings' => 'lwp-footer-callout-image',
+			'width' => 750,
+			'height' => 500
+		)));
 }
 
-add_action('customize_register','lwp_footer_callout');
+add_action('customize_register', 'lwp_footer_callout');
--- wp-content/themes/learningWordPress/index.php
+++ wp-content/themes/learningWordPress/index.php
@@ -8,12 +8,23 @@ get_header(); ?>
 		<!-- main-column -->
 		<div class="main-column">
 
+			<?php if (current_user_can('administrator')) : ?>
+			<div class="admin-quick-add">
+				<h3>Quick Add Post</h3>
+				<input type="text" name="title" placeholder="Title">
+				<textarea name="content" placeholder="Content"></textarea>
+				<button id="quick-add-button">Create Post</button>
+			</div>
+			<?php endif; ?>
+
 			<?php if (have_posts()) :
 				while (have_posts()) : the_post();
 
 				get_template_part('content', get_post_format());
 
 				endwhile;
+
+				echo paginate_links();
 
 				else :
 					echo '<p>No content found</p>';
